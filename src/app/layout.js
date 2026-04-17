e6deb3a2e53616804b11cebde40bb8e259a40b80
Etarnity Global <programmer.fahim.online@gmail.com>
2026-04-17 19:35:02 +0600

    Created Layout

---
+++ src/app/layout.js
@@ -0,0 +1,38 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/Footer";
+import ToastProvider from "@/Components/ToastProvider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Keen Keeper",
+  description: "Track friendships, log interactions, and stay connected with your closest people.",
+  datatheme: "light",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      data-theme="light"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+        <ToastProvider />
+      </body>
+    </html>
+  );
+}
